51f9f7b6aba78b2e0aebaefa9b02140f25544bcd
MicroJack <zx.micro.jack@gmail.com>
2025-07-09 22:22:34 +0100

    Added version

--- spectrum/zxtres/mkrelease.sh
+++ spectrum/zxtres/mkrelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp ./zxspectrum.runs/impl_1_a200t/zx3top.bit ZXSpectrum-MiST-zx3-a200t.bit
-cp ./zxspectrum.runs/impl_1/zx3top.bit ZXSpectrum-MiST-zx3-a35t.bit
-cp ./zxspectrum.runs/impl_1_a100t/zx3top.bit ZXSpectrum-MiST-zx3-a100t.bit
-zip ZXSpectrum-MiST-zx3-v1.1.zip *.bit readme.txt spectrum.rom
+cp -v ./zxspectrum.runs/impl_1_a200t/zx3top.bit ZXSpectrum-MiST-zx3-a200t.bit
+cp -v ./zxspectrum.runs/impl_1/zx3top.bit ZXSpectrum-MiST-zx3-a35t.bit
+cp -v ./zxspectrum.runs/impl_1_a100t/zx3top.bit ZXSpectrum-MiST-zx3-a100t.bit
+zip ZXSpectrum-MiST-zx3-v1.2.zip *.bit readme.txt spectrum.rom
 
--- spectrum/zxtres/readme.txt
+++ spectrum/zxtres/readme.txt
@@ -1,4 +1,5 @@
 Versions:
 
+1.2 - (ZXTres) Added support for 6 button mouse
 1.1 - (ZXTres) Added audio input.
 1.0 - ZX Tres working with AY
